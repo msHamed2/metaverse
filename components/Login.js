f4d5581bc2ebf5ca75533b63aa7ffc7e6a412bcf
mahmoud hamed <mahmoud.hamed@tatweer.sy>
2021-12-20 19:52:45 +0200

    login metamask

--- components/Login.js
+++ components/Login.js
@@ -1,19 +1,32 @@
 import Image from "next/image";
+import { useMoralis } from "react-moralis";
+
 
 function Login() {
+  const {authenticate} = useMoralis();
   return (
-    <div className="bg-black relative text-white">
-      <h1> hello this is login</h1>
-      <div className="absolute z-50 space-y-6 flex flex-col h-4/6 w-full items-center justify-center">
-        <Image className="rounded-full object-cover" src="https://links.papareact.com/3pi" height={200} width={200} />
-        <button className="bg-yellow-500 p-2 font-bold rounded-lg animate-pulse text-slate-600"> Login to the world</button>
+    <div className=" bg-black relative text-white ">
+     
+
+      <div className="flex flex-col absolute z-50 w-full h-4/6  items-center justify-center space-y-4">
+          <Image 
+          className="object-cover rounded-full"
+           src="https://links.papareact.com/3pi"
+          width={200} height={200}
+           />
+           <button
+           onClick={authenticate}
+            className="text-black bg-yellow-300 rounded-lg p-5 font-bold animate-pulse">
+              Login to The MetaVerse 
+              </button>
+
       </div>
       <div className="w-full h-screen">
-        <Image
-          src="https://links.papareact.com/55n"
-          layout="fill"
+          <Image 
+          src="https://links.papareact.com/55n" 
+          layout='fill' 
           objectFit="cover"
-        />
+           />
       </div>
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
-
+import { MoralisProvider } from "react-moralis";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <MoralisProvider appId="tyLFC1fmpdQY3I6rDFkvoGqwlEUQXgkcQxSrhft7" serverUrl="https://wvall52zxfp2.usemoralis.com:2053/server">
+
+  <Component {...pageProps} />
+  </MoralisProvider>
+  )
 }
 
 export default MyApp
