ee1c7251ac01f4e965e8901586c5786a2641946f
mrbeastmahmoud <mahmoudsu22@gmail.com>
2021-12-20 14:22:48 +0200

    Login commit

--- components/Login.js
+++ components/Login.js
@@ -1,9 +1,22 @@
+import Image from "next/image";
+
 function Login() {
-    return (
-        <div>
-            <h1> hello this is login</h1>
-        </div>
-    )
+  return (
+    <div className="bg-black relative text-white">
+      <h1> hello this is login</h1>
+      <div className="absolute z-50 space-y-6 flex flex-col h-4/6 w-full items-center justify-center">
+        <Image className="rounded-full object-cover" src="https://links.papareact.com/3pi" height={200} width={200} />
+        <button className="bg-yellow-500 p-2 font-bold rounded-lg animate-pulse text-slate-600"> Login to the world</button>
+      </div>
+      <div className="w-full h-screen">
+        <Image
+          src="https://links.papareact.com/55n"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,15 @@ import Head from "next/head";
 import Login from "../components/Login";
 
 export default function Home() {
+  const isAuthenticated = false;
+if( !isAuthenticated ) return (<Login/>);
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="h-screen ">
       <Head>
         <title>Meta verse dApp</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Login />
+      <h1>wellcome to the metaverse</h1>
     </div>
   );
 }
