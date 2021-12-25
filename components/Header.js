d1d74761f194cc0542fa36ba1a1698e860f05591
mahmoud hamed <mahmoud.hamed@tatweer.sy>
2021-12-25 11:09:19 +0200

    messages components

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ import ChangeUsername from "./ChangeUsername";
 function Header() {
   const { user } = useMoralis();
   return (
-    <div className="text-pink-500">
+    <div className="sticky top-0 p-5 z-50 bg-black shadow-sm border-b-2 border-pink-700 text-pink-500">
       <div className="grid grid-cols-5 lg:grid-cols-6 items-end lg:items-center">
         <div className="relative h-24 w-24 mx-auto hidden lg:inline-grid">
           <Image
